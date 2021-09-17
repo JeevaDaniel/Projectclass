398181f99e5ab020687d6a67396ac91dfb80140b
JeevaDaniel <jeevadaniel02@gmail.com>
2021-09-17 11:06:36 +0530

    B works

--- src/org/run/Project.java
+++ src/org/run/Project.java
@@ -1,5 +1,13 @@
 package org.run;
 
 public class Project {
+	
+	
+public static void main(String[] args) {
+     System.out.println("good");
+     System.out.println("bad");
+     System.out.println("worst");
+}
+	
 
 }
